6a4c42669777805f36baf45709d4b9dc894a37d8
Prasanna <Prasanna.kumar@lendingkart.com>
2023-06-12 14:48:12 +0530

    updated creds

--- trinoConnect.py
+++ trinoConnect.py
@@ -3,7 +3,7 @@ from trino.dbapi import connect
 from trino.auth import BasicAuthentication
 
 #%%
-conn = connect(host='prod-trino.lendingkart.com',port=443,http_scheme='https',catalog='delta',auth=BasicAuthentication('sqlreadonly','GxksfFX4HnmIeDzUfC7n'),verify=True)
+conn = connect(host='<prod trino host>',port=443,http_scheme='https',catalog='delta',auth=BasicAuthentication('sqlreadonly','<password>'),verify=True)
 cur = conn.cursor(legacy_primitive_types=True)
 cur.execute("select * from delta.analytics.applications limit 10")
 rows = cur.fetchall()
